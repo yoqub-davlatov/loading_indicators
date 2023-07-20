ccd7b5e9ea13ac325df4cc8e268586e1eff3d7ab
Habibullo-777 <habibullo.assoev@gmail.com>
2023-07-20 12:24:59 +0300

    Updating.

--- lib/animated_loading_indicators.dart
+++ lib/animated_loading_indicators.dart
@@ -8,5 +8,4 @@ export 'package:animated_loading_indicators/src/pacman.dart';
 export 'package:animated_loading_indicators/src/text_loader.dart';
 export 'package:animated_loading_indicators/src/yingyang.dart';
 export 'package:animated_loading_indicators/src/circle_loader.dart';
-export 'package:animated_loading_indicators/src/circles.dart';
 
